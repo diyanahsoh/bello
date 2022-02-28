9a8e727159be549bfae39ece563d8d3566a948df
Diyanah <diyanahsoh@gmail.com>
2022-02-28 13:45:57 +0800

    removed unused code and commented out edit icon

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,7 @@
 <template>
     <b-list-group-item class="d-flex justify-content-between align-items-center cursor-move">
-        <!-- <b-form-textarea id="textarea" :value="cardDescription" placeholder="Add Card" rows="1" max-rows="4">
-        </b-form-textarea> 
-        <b-form-textarea id="textarea-plaintext" class="card-text" plaintext :value="cardText"></b-form-textarea>-->
         <p class="ml-3 card-text">{{ cardText }}</p>
-        <b-icon icon="pencil-square" scale="2" variant="success"></b-icon>
+        <!-- <b-icon icon="pencil-square" scale="2" variant="success" @click="$emit('on-edit', cardText, board)"></b-icon> -->
         <b-icon icon="trash" scale="2" variant="danger" @click="$emit('on-delete', cardText, board)"></b-icon>
     </b-list-group-item> 
 </template>
@@ -21,11 +18,6 @@
         cardText: this.name
       }
     },
-    methods: {
-      onEdit(user) {
-        alert(`Editing ${user.name}`);
-      }
-    }
   }
 </script>
 
