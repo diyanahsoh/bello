1d9aa1f964cd7ec199b75059d2aca10ef597ec8c
Diyanah <diyanahsoh@gmail.com>
2022-03-01 14:27:59 +0800

    Added Edit Max Card functionality and minor style edits

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container vh-100">
     <div class="row">
-      <h1 class="mt-3">Bello - Kanban Board</h1>
+      <div class="col-12"><h1 class="mt-3">Bello - Kanban Board</h1></div>
       <div class="col">
         <b-form-input id="inline-form-input-name" class="mb-2 mr-sm-2 mb-sm-0" v-model="name" placeholder="Board Name"
           style="font-size: 30px"></b-form-input>
@@ -13,12 +13,12 @@
     </div>
     <div class="row mt-4">
       <div class="col">
-        <div class="p-3 alert alert-primary">
+        <div class="p-3 alert-primary">
           <h3>To-Do</h3>
           <draggable class="list-group kanban-column" :list="arrTodo" group="tasks">
             <Card v-for="cards in arrTodo" :key="cards.name" :name="cards.name" :board="arrTodo" @on-edit="onEdit" @on-delete="onDelete" />
           </draggable>
-          <b-form inline v-if="addTodo">
+          <b-form inline v-if="addTodo" class="mt-3">
             <b-form-input id="inline-form-input-name" class="mb-2 mr-sm-2 mb-sm-0" v-model="todoCard"
               placeholder="Enter Task" style="display: inline; width: inherit; margin-right: 20px;">
             </b-form-input>
@@ -26,20 +26,32 @@
               Add
             </b-button>
           </b-form>
-          <b-button v-else variant="primary" class="ml-3" @click="addTodo=true">
+          <b-button v-else variant="primary" class="mt-3" @click="addTodo=true">
             Add new card
           </b-button>
         </div>
-        <b-alert show variant="primary">Max No. Cards: {{ maxTodo }}</b-alert>
+        <b-alert show variant="primary">Max No. Cards: {{ maxTodo }}
+          <b-form inline v-if="editTodo">
+            <b-form-input class="mb-2 mr-sm-2 mb-sm-0" v-model="todoNum"
+              placeholder="Num" style="max-width: 100px;" type="number">
+            </b-form-input>
+            <b-button variant="success" @click="maxTodo=todoNum, todoNum='', editTodo=false">
+              Confirm
+            </b-button>
+          </b-form>
+          <b-button v-else variant="info" class="ml-3" @click="editTodo=true">
+            Edit Max No.
+          </b-button>
+        </b-alert>
       </div>
 
       <div class="col">
-        <div class="p-3 alert alert-warning">
+        <div class="p-3 alert-warning">
           <h3>In Progress</h3>
           <draggable class="list-group kanban-column" :list="arrInProgress" group="tasks">
             <Card v-for="cards in arrInProgress" :key="cards.name" :name="cards.name" :board="arrInProgress" @on-edit="onEdit" @on-delete="onDelete" />
           </draggable>
-          <b-form inline v-if="addProg">
+          <b-form inline v-if="addProg" class="mt-3">
             <b-form-input id="inline-form-input-name" class="mb-2 mr-sm-2 mb-sm-0" v-model="progCard"
               placeholder="Enter Task" style="display: inline; width: inherit; margin-right: 20px;">
             </b-form-input>
@@ -47,32 +59,56 @@
               Add
             </b-button>
           </b-form>
-          <b-button v-else variant="primary" class="ml-3" @click="addProg=true">
+          <b-button v-else variant="primary" class="mt-3" @click="addProg=true">
             Add new card
           </b-button>
         </div>
-        <b-alert show variant="warning">Max No. Cards: {{ maxProg }}</b-alert>
+        <b-alert show variant="warning">Max No. Cards: {{ maxProg }}
+          <b-form inline v-if="editProg">
+            <b-form-input class="mb-2 mr-sm-2 mb-sm-0" v-model="progNum"
+              placeholder="Num" style="max-width: 100px;" type="number">
+            </b-form-input>
+            <b-button variant="success" @click="maxProg=progNum, progNum='', editProg=false">
+              Confirm
+            </b-button>
+          </b-form>
+          <b-button v-else variant="info" class="ml-3" @click="editProg=true">
+            Edit Max No.
+          </b-button>
+        </b-alert>
       </div>
 
       <div class="col">
-        <div class="p-3 alert alert-success">
+        <div class="p-3 alert-success">
           <h3>Done</h3>
           <draggable class="list-group kanban-column" :list="arrDone" group="tasks">
             <Card v-for="cards in arrDone" :key="cards.name" :name="cards.name" :board="arrDone" @on-edit="onEdit" @on-delete="onDelete" />
           </draggable>
-          <b-form inline v-if="addDone">
-            <b-form-input id="inline-form-input-name" class="mb-2 mr-sm-2 mb-sm-0" v-model="doneCard"
+          <b-form inline v-if="addDone" class="mt-3">
+            <b-form-input class="mb-2 mr-sm-2 mb-sm-0" v-model="doneCard"
               placeholder="Enter Task" style="display: inline; width: inherit; margin-right: 20px;">
             </b-form-input>
             <b-button variant="success" class="ml-3" @click="addCard()">
               Add
             </b-button>
           </b-form>
-          <b-button v-else variant="primary" class="ml-3" @click="addDone=true">
+          <b-button v-else variant="primary" class="mt-3" @click="addDone=true">
             Add new card
           </b-button>
         </div>
-        <b-alert show variant="success">Max No. Cards: {{ maxDone }}</b-alert>
+        <b-alert show variant="success">Max No. Cards: {{ maxDone }}
+          <b-form inline v-if="editDone">
+            <b-form-input class="mb-2 mr-sm-2 mb-sm-0" v-model="doneNum"
+              placeholder="Num" style="max-width: 100px;" type="number">
+            </b-form-input>
+            <b-button variant="success" @click="maxDone=doneNum, doneNum='', editDone=false">
+              Confirm
+            </b-button>
+          </b-form>
+          <b-button v-else variant="info" class="ml-3" @click="editDone=true">
+            Edit Max No.
+          </b-button>
+        </b-alert>
       </div>
     </div>
     <KanbanColumn />
@@ -104,6 +140,9 @@ export default {
       maxTodo: 5,
       maxProg: 5,
       maxDone: 5,
+      editTodo: false,
+      editProg: false,
+      editDone: false,
       arrTodo: [
         { name: "Code Sign Up Page" },
         { name: "Help with Designs" }
